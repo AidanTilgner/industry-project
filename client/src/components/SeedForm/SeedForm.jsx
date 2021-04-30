7f68764564add36573c470607b07e7185021f9e1
hemcrae <halley.mcrae@gmail.com>
2021-04-30 02:45:43 -0400

    server.js and seedform.jsx

--- client/src/components/SeedForm/SeedForm.jsx
+++ client/src/components/SeedForm/SeedForm.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './SeedForm.scss';
+import './server/library.json';
 
 export default class SeedForm extends Component { 
 
     state = {
+        words: [],
+    }
 
+    componentDidMount() {
+        axios
+            .get('http://localhost:8080')
+            .then((data) => {
+                this.setState({
+                    words: data
+                })
+            })
     }
 }
